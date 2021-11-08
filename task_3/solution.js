b24de6c2e55a38b0d9f88498a5c7068fc669c559
dispego <47778253+dispego@users.noreply.github.com>
2021-11-08 19:18:32 +0300

    Update solution.js
    fix

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,31 +3,33 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
+    // product - объект вида {id: id, title: title, count: count, price: price}
+    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
+    // id - идентификатор
+    // title - наименование
+    // price - цена
+    // count - количество
+
+    // сформируйте строку itemCountText вида:
+    // количество × цена ₽ = стоимость ₽
+    // например,
+    // 5 × 500.00 ₽ = 2500 ₽
+
+    // 1. Создайте переменную itemCountText, равную пустой строке
     let itemCountText = '';
+    // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     itemCountText  = itemCountText + product.count;
+    // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     itemCountText = itemCountText + ` × `;
+    // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
     itemCountText = itemCountText + product.price;
+    // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
     itemCountText = itemCountText + ` ₽ = `;
+    // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     let sum = product.price * product.count;
+    // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     itemCountText = itemCountText + sum;
-    itemCountText = itemCountText + ` ₽`;
-
-    // Конец решения задания №3.1.
-
-    return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
-}
-    
-    itemCountText  = itemCountText + product.count;
-    itemCountText = itemCountText + ` × `;
-   
-    itemCountText = itemCountText + product.price;
-    
-    itemCountText = itemCountText + ` ₽ = `;
-    
-    let sum = product.price * product.count;
-    
-    itemCountText = itemCountText + sum;
-    
+    // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
     itemCountText = itemCountText + ` ₽`;
 
     // Конец решения задания №3.1.
