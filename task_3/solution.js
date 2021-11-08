4dd1cae8caa539d15f35416aa63c154b21167933
dispego <47778253+dispego@users.noreply.github.com>
2021-11-08 19:15:07 +0300

    Update solution.js
    выполнено

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,8 +3,19 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
-   
     let itemCountText = '';
+    itemCountText  = itemCountText + product.count;
+    itemCountText = itemCountText + ` × `;
+    itemCountText = itemCountText + product.price;
+    itemCountText = itemCountText + ` ₽ = `;
+    let sum = product.price * product.count;
+    itemCountText = itemCountText + sum;
+    itemCountText = itemCountText + ` ₽`;
+
+    // Конец решения задания №3.1.
+
+    return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
+}
     
     itemCountText  = itemCountText + product.count;
     itemCountText = itemCountText + ` × `;
