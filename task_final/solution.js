d24fd34e18fc0b951e132270f7d1a07e0ee79265
dispego <47778253+dispego@users.noreply.github.com>
2021-11-09 10:22:28 +0300

    Update solution.js
    fix comment

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,13 +34,13 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: "", order: {}, goods: []};
+    let data = {order: {}, goods: []};
     
     let countOfGoods = goods.length;
     
-        for (let i = 0; i < countOfGoods; i += 1) {
-            data.goods.push({title: goods[i].title, count: goods[i].count});
-        }
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
+    }
     
     data.order.address = "ул. "+ address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв "+ address.flat;
     data.order.sum = sum;
